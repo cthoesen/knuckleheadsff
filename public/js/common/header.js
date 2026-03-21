29df1f02b94c5f42344475892db600020395f278
cthoesen <cthoesen@gmail.com>
2026-03-20 20:43:59 -0400

    bumped the revision to 12 and re-minified header.min.js. Once you deploy this, it will force all browsers to re-fetch the sprite file and the icon should appear.

--- public/js/common/header.js
+++ public/js/common/header.js
@@ -6434,7 +6434,7 @@ function scoreAdjustmentHtml(which) {
 (function (window, document) {
 	'use strict';
 	const file = 'https://mfl-leagues.com/images/shared/icons/svg-sprites.svg';
-	const revision = 11;
+	const revision = 12;
 
 	if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) {
 		return true;
