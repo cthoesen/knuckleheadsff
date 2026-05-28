dcd7e7a1c0f62d0159fb5fbb4987cdcc425fbc5d
cthoesen <cthoesen@gmail.com>
2026-05-28 16:40:43 -0400

    added new chat cool icon for MFL chat room

--- public/js/common/header.js
+++ public/js/common/header.js
@@ -6464,7 +6464,7 @@ function scoreAdjustmentHtml(which) {
 (function (window, document) {
 	'use strict';
 	const file = 'https://knuckleheadsff.com/images/shared/icons/svg-sprites.svg';
-	const revision = 26; // Change this value to invalidate cache
+	const revision = 27; // Change this value to invalidate cache
 
 	if (!document.createElementNS || !document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect) {
 		return true;
